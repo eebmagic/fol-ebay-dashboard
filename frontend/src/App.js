bdb06483498d6fc952a50fcc4c67df09cf1a8add
E Bolton <eebmagic@gmail.com>
2024-12-02 23:20:07 -0600

    add catch for failed to get code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,26 @@
+import { useState, useEffect } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
-import { useState, useEffect } from 'react';
 
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isFailed, setIsFailed] = useState(false);
 
   useEffect(() => {
     const params = new URLSearchParams(window.location.search);
     if (params.get('isAuthSuccessful') === 'true' && params.get('code')) {
       handleSuccessfulAuth(params.get('code'));
+    } else if (params.get('isAuthSuccessful') === 'false') {
+      setIsFailed(true);
     }
   }, []);
 
   const handleSuccessfulAuth = async (code) => {
     console.log('Got EBay code');
     console.log(code);
+
+    // TODO: Send code to backend to get sessionId
 
     setIsAuthenticated(true);
   }
@@ -31,6 +37,11 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         {isAuthenticated ? (
           <p style={{color: 'green'}}><i>Successfully authenticated with eBay!</i></p>
+        ) : isFailed ? (
+          <div>
+            <p style={{color: 'red'}}><i>Failed to authenticate with eBay!</i></p>
+            <p>Try clearing out the url and signing in again.</p>
+          </div>
         ) : (
           <>
             <p>Click the button below to sign in with eBay</p>
